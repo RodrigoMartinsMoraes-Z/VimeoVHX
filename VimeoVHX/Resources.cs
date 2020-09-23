d19c036c8f10e50d61ddb91a32569d1dce90874a
Rodrigo Martins Moraes <rodrigo_moraes@hotmail.com.br>
2020-09-23 16:44:37 -0300

    resources of products

--- VimeoVHX/Resources.cs
+++ VimeoVHX/Resources.cs
@@ -1,9 +1,14 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using VimeoVHX.Product;
 
 namespace VimeoVHX
 {
@@ -33,5 +38,52 @@ namespace VimeoVHX
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes($"{token}:")));
         }
 
+        public async Task<Product.Product> RetrieveAProduct(int href)
+        {
+            if (href <= 0)
+            {
+                throw new Exception("Href can't be null or less than 1");
+            }
+
+            HttpResponseMessage response = await _client.GetAsync("https://api.vhx.tv/products/" + href);
+
+            return (Product.Product)await ConvertToObject(response, new Product.Product());
+
+        }
+
+        public async Task<ListOfProducts> ListOfProducts(ListProductParams productParams)
+        {
+
+            string queryString = await GenerateQueryString(productParams);
+
+            HttpResponseMessage response = await _client.GetAsync("https://api.vhx.tv/products/" + queryString);
+
+            return (ListOfProducts)await ConvertToObject(response, new ListOfProducts());
+        }
+
+        private async Task<object> ConvertToObject(HttpResponseMessage responseMessage, object obj)
+        {
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                string content = await responseMessage.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<object>(content);
+            }
+            else
+                throw NotSuccessCode(responseMessage);
+        }
+
+        private Task<string> GenerateQueryString(object obj)
+        {
+            IEnumerable<string> properties = from p in obj.GetType().GetProperties()
+                                             where p.GetValue(obj, null) != null
+                                             select p.Name + "=" + HttpUtility.UrlEncode(p.GetValue(obj, null).ToString());
+
+            return Task.FromResult(string.Join("&", properties.ToArray()));
+        }
+
+        private Exception NotSuccessCode(HttpResponseMessage response)
+        {
+            return new Exception("Error, tried to get a successful status code, but get: " + response.StatusCode.ToString());
+        }
     }
 }
